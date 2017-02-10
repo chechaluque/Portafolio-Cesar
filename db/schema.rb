2df6ca61e2743c8eb850ff5831bc94f1a97a1b9f
chechaluque <cesarluque2000@gmail.com>
2017-02-09 18:43:24 -0800

    Fix the first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208061923) do
+ActiveRecord::Schema.define(version: 20170210023544) do
 
   create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
